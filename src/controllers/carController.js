f3ed707d061cf70e15f0c5efce2fa37d5dae56a6
daviddangtim <132236718+daviddangtim@users.noreply.github.com>
2024-07-15 12:47:16 +0100

    Image Uploading

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -1,15 +1,38 @@
 import { catchAsync } from "../utils/helpers.js";
 import Car from "../models/carModel.js";
-import {
-  cloudinary,
-  cloudinaryImageUpdater,
-  cloudinaryImageUploader,
-} from "../utils/imageUploader.js";
+import { cloudinary, cloudinaryImageUploader } from "../utils/imageUploader.js";
 import AppError from "../utils/appError.js";
 import * as service from "../services/carService.js";
 import * as factory from "../controllers/handlerFactory.js";
 
 import Dealership from "../models/dealershipModel.js";
+
+
+
+export const createCarV1 = catchAsync(async (req, res, next) => {
+  const userId = req.user.id;
+  const dealership = await Dealership.findOne(
+      { car: { _id: userId } },
+      { _id: 1 },
+      { lean: true },
+  ).exec();
+  if (!dealership) {
+    return next(new AppError("No dealership found with this ID", 404));
+  }
+  const payload = filterObject(req.body, ["slug", "isAvailable"], {
+    exclude: true,
+  });
+  
+  payload.dealership = dealership._id;
+  payload.images = req.photos;
+  payload.imageIds = req.photosId;
+  const car = await Car.create(payload);
+
+  res.status(201).json({
+    statusText: "success",
+    data: { car },
+  });
+});
 
 export const setDealershipId = (req, res, next) => {
   if (!req.body.dealership) req.body.dealership = req.params.id;
@@ -21,8 +44,13 @@ export const getAllCars = factory.getAll(service.getAllCars, (req) => {
   return { filter };
 });
 
-export const createCar = factory.createOne(
+export const setCoverPhoto = catchAsync(async (req, res, next) => {
+  const result = req.user.photo
+    ? await cloudinaryImageUpdater(req.file.buffer, req.user.photo.id)
+    : await cloudinaryImageUploader(req.file.buffer)
+})
 
+export const createCar = factory.createOne(
   service.createCar
 );
 export const getCar = factory.getById(service.getCar);
